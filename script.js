f4abcc5c65858004eaea8df3ba677d421fab028d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-03-18 08:17:38 +0100

    a little more verbose + misc tweaks

--- script.js
+++ script.js
@@ -1,73 +1,136 @@
-function fetchData(endpoint) {
-  const apiUrl = document.getElementById("apiUrl").value;
-  if (!apiUrl) {
-    alert("Veuillez entrer l'URL de l'API.");
-    return;
+document.addEventListener("DOMContentLoaded", () => {
+
+  function ok() {
+    const resultsElt = document.getElementById("results");
+    resultsElt.classList.add("ok");
+    resultsElt.classList.remove("ko");
   }
 
-  fetch(`${apiUrl}${endpoint}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Réponse réseau non ok.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      document.getElementById("results").textContent = JSON.stringify(
-        data,
-        null,
-        2
-      );
-    })
-    .catch((error) => console.error("Erreur de fetch:", error));
-}
-
-function fetchDataWithId(endpoint, idFieldId, suffix = "") {
-  const apiUrl = document.getElementById("apiUrl").value;
-  const itemId = document.getElementById(idFieldId).value;
-  if (!apiUrl) {
-    alert("Veuillez entrer l'URL de l'API.");
-    return;
-  }
-  if (!itemId) {
-    alert("Veuillez entrer un ID.");
-    return;
+  function ko() {
+    const resultsElt = document.getElementById("results");
+    resultsElt.classList.add("ko");
+    resultsElt.classList.remove("ok");
   }
 
-  fetch(`${apiUrl}${endpoint}${itemId}${suffix}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Réponse réseau non ok.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      document.getElementById("results").textContent = JSON.stringify(
-        data,
-        null,
-        2
-      );
-    })
-    .catch((error) => console.error("Erreur de fetch:", error));
-}
+  function fetchData(endpoint) {
+    const apiUrl = document.getElementById("apiUrl").value;
+    if (!apiUrl) {
+      alert("Veuillez entrer l'URL de l'API.");
+      ko();
+      return;
+    }
 
-const isAlive = () => {
-  const apiUrl = document.getElementById("apiUrl").value;
-  fetch(`${apiUrl}/api/alive`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Réponse réseau non ok.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.message === "Alive") {
-        document.getElementById("aliveStatus").textContent = "✅";
-      }
-    })
-    .catch((error) => {
-      document.getElementById("aliveStatus").textContent = "❌";
-    });
-};
+    const fetchUrl = `${apiUrl}${endpoint}`
+    console.log(`using URL ${fetchUrl}`)
+    fetch(fetchUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Réponse réseau non ok.");
+          ko();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        document.getElementById("results").textContent = JSON.stringify(
+          data,
+          null,
+          2
+        );
+        ok();
+      })
+      .catch((error) => {
+        console.error("Erreur de fetch:", error);
+        ko();
+      })
+  }
 
-document.getElementById("apiUrl").addEventListener("change", isAlive);
+  function fetchDataWithId(endpoint, idFieldId, suffix = "") {
+    const apiUrl = document.getElementById("apiUrl").value;
+    const itemId = document.getElementById(idFieldId).value;
+    if (!apiUrl) {
+      alert("Veuillez entrer l'URL de l'API.");
+      ko();
+      return;
+    }
+    if (!itemId) {
+      alert("Veuillez entrer un ID.");
+      ko();
+      return;
+    }
+
+    const fetchUrl = `${apiUrl}${endpoint}${itemId}${suffix}`
+    console.log(`using URL ${fetchUrl}`)
+    fetch(fetchUrl)
+      .then((response) => {
+        if (! response.ok) {
+          throw new Error(`Réponse réseau non ok: ${response.status}`);
+          ko();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        document.getElementById("results").textContent = JSON.stringify(
+          data,
+          null,
+          2
+        );
+        ok();
+      })
+      .catch((error) => {
+        console.error("Erreur de fetch:", error);
+        ko();
+      })
+  }
+
+  function refreshAlive(event) {
+    const apiUrl = document.getElementById("apiUrl").value;
+    const aliveUrl = `${apiUrl}/api/alive`;
+    console.log(`checking ${aliveUrl}`);
+    const aliveStatusElement = document.getElementById("aliveStatus");
+    const isAlive = () => {
+      aliveStatusElement.textContent = "✅";
+    };
+    const isDead = () => {
+      aliveStatusElement.textContent = "❌";
+    };
+    aliveStatusElement.textContent = "⏳";
+    fetch(aliveUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Réponse réseau non ok.");
+          isDead();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.message === "Alive") {
+          isAlive();
+        } else {
+          console.error("Erreur de fetch:", data);
+          isDead();
+        }
+      })
+      .catch((error) => {
+        isDead();
+      });
+  }
+
+  document.getElementById("apiUrl").addEventListener("change", refreshAlive);
+  document
+    .getElementById("listAssoc")
+    .addEventListener("click", () => fetchData("/api/associations"));
+  document
+    .getElementById("listEvent")
+    .addEventListener("click", () => fetchData("/api/evenements"));
+  document
+    .getElementById("detailAssoc")
+    .addEventListener("click", () => fetchDataWithId('/api/association/', 'itemId'));
+  document
+    .getElementById("detailEvent")
+    .addEventListener("click", () => fetchDataWithId('/api/evenement/', 'itemId'));
+  document
+    .getElementById("assocEvents")
+    .addEventListener("click", () => fetchDataWithId('/api/association/', 'itemId', '/evenements'));
+
+  refreshAlive();
+});
